a38592e6e879d5f7b1a1f189ae956badf8dbd57e
Jéssica Beatriz <jessicabeatriz.dev@hotmail.com>
2026-01-26 22:24:06 -0300

    feat: criação da navbar e do componente ThemeToggleBtn para alteração do tema do site

---
+++ src/assets/assets.js
@@ -0,0 +1,21 @@
+import light_logo from './light-logo.svg'
+import dark_logo from './dark-logo.svg'
+import arrow_icon from './arrow_icon.svg'
+import close_icon from './close_icon.svg'
+import menu_icon_light from './menu_icon_light.svg'
+import menu_icon_dark from './menu_icon_dark.svg'
+import sun_icon from './sun_icon.svg'
+import moon_icon from './moon_icon.svg'
+
+const assets = {
+    light_logo,
+    dark_logo,
+    arrow_icon,
+    close_icon,
+    menu_icon_light,
+    menu_icon_dark,
+    sun_icon, 
+    moon_icon
+}
+
+export default assets
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+<<<<<<< HEAD
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+=======
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
+>>>>>>> 85fbd1e (feat: criação da navbar e do componente ThemeToggleBtn para alteração do tema do site)
 })
